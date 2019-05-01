48b8ccc7e09fe924850a8c508d778ae278b6437f
wassi18 <48069149+wassi18@users.noreply.github.com>
2019-05-01 23:51:49 +0200

    Add files via upload

---
+++ úkol 5.m
@@ -0,0 +1,22 @@
+function varargout = samostatna_prac_5(varargin)
+
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @samostatna_prac_5_OpeningFcn, ...
+                   'gui_OutputFcn',  @samostatna_prac_5_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+
+
+
+
